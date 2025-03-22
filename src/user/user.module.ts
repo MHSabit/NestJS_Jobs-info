264fc0acc9b625fc8673b350832efa7d87e06218
MHSabit <mahmudhassansabit@gmail.com>
2025-03-23 03:42:11 +0600

    add

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,11 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { Profile } from 'src/profile/profile.entity';
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Profile])],
 })
 export class UserModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,18 +2,27 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
+import { Profile } from 'src/profile/profile.entity';
 
 @Injectable()
 export class UserService {
     constructor(
     @InjectRepository(User)
-    private jobsRepository: Repository<User>
-    ) {
+    private jobsRepository: Repository<User>,
 
-    }
+    @InjectRepository(Profile)
+    private profileRepository: Repository<Profile>,
+    ) {}
 
 
     createUser() {
+        // create a Prfile & save 
+
+
+        // set the profile id to the user
+
+
+        // save the user Object
         return 'User Created';
     }
 }
